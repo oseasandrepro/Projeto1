cd7e29dbe751f702f5e92e50fb65244b245d6ed6
oseasandrepro <oseasx65@gmail.com>
2021-10-02 16:27:04 -0300

    resize, edit, z-order, OK

--- Projeto1App.java
+++ Projeto1App.java
@@ -1,8 +1,14 @@
+import java.util.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.Point;
 
 import figures.*;
 
@@ -17,17 +23,25 @@ class Projeto1App
 
 class ListFrame extends JFrame
 {
+
+	Point2D[] lastPoints = new Point2D[3];	
+	
 	Point pointOfmouse = null;
 	Figure selectedFigure = null;
 	Figure auxFigure = null;
 	Point prevPt = null;
+	int pos = -1;
 	
+	int SIZE = 8;
 	int defaultH = 80;
 	int defaultW = 80;
 	
 	int windowH = 500;
 	int windowW = 500;
 	
+	Rectangle2D.Double[] points = { new Rectangle2D.Double(50, 50,SIZE, SIZE), 
+                                   new Rectangle2D.Double(150, 100,SIZE, SIZE),
+                                   new Rectangle2D.Double(100, 75,SIZE, SIZE)};
 	
 	ArrayList<Figure> fg = new ArrayList<Figure>();
 	Random rand = new Random();
@@ -80,6 +94,50 @@ class ListFrame extends JFrame
 						repaint();
 					}
 				}
+				else if( evt.getKeyChar() == 'f')
+				{
+					if( selectedFigure != null){
+						Color newColor = JColorChooser.showDialog(null, "Choose a color", selectedFigure.BorderColor);
+						selectedFigure.BorderColor = newColor;
+						repaint();
+					}
+				}
+				else if( evt.getKeyChar() == 'b')
+				{
+					if( selectedFigure != null){
+						Color newColor = JColorChooser.showDialog(null, "Choose a color", selectedFigure.BorderColor);
+						selectedFigure.BckgColor = newColor;
+						repaint();
+					}
+				}
+				else if (evt.getKeyCode() == KeyEvent.VK_UP) 
+				{
+					if( selectedFigure != null){
+						selectedFigure.y -= 1;
+						repaint();
+					}
+				}
+				else if(evt.getKeyCode() == KeyEvent.VK_DOWN) 
+				{
+					if( selectedFigure != null ){
+						selectedFigure.y += 1;
+						repaint();
+					}
+				}
+				else if (evt.getKeyCode() == KeyEvent.VK_LEFT) 
+				{
+					if( selectedFigure != null){
+						selectedFigure.x -= 1;
+						repaint();
+					}
+				}
+				else if (evt.getKeyCode() == KeyEvent.VK_RIGHT) 
+				{
+					if( selectedFigure != null){
+						selectedFigure.x += 1;
+						repaint();
+					}
+				}
 			}
 		}
 			
@@ -90,57 +148,132 @@ class ListFrame extends JFrame
 			 public void mousePressed(MouseEvent evt) 
 			 {
 			  prevPt = evt.getPoint();
-			  for( Figure fig: fg)
+			 
+			  if( selectedFigure == null)
 			  {
-				if( (prevPt.getX()>=fig.x && prevPt.getX()<=fig.x+fig.w) && (prevPt.getY()>=fig.y && prevPt.getY()<=fig.y+fig.h) ){
-					selectedFigure = fig;
-					break;
-				}
-				else
-				{
-					selectedFigure = null;
-					auxFigure = null;
-				}
-			  }
+			  	ListIterator<Figure> li = fg.listIterator(fg.size());
+			  	Figure fig = null;
+			  	while( li.hasPrevious() )
+			 	{
+			 		fig = li.previous();
+				 	if( (prevPt.getX()>=fig.x && prevPt.getX()<=fig.x+fig.w) && (prevPt.getY()>=fig.y && prevPt.getY()<=fig.y+fig.h) )
+				 	{
+						selectedFigure = fig;
+						int index = fg.indexOf(selectedFigure);
+						fg.remove(index);
+						fg.add(selectedFigure);
+						break;
+					}
+					else
+					{
+					  selectedFigure = null;
+					  auxFigure = null;
+				       }
+        		 	}
+			 }
+			 else
+			 {
+			 	boolean flag = false;
+			 	for (int i = 0; i < points.length; i++) 
+			 	{
+					if (points[i].contains(prevPt)) 
+					{
+					  pos = i;
+					  flag = true;
+					  
+					  //System.out.printf("point %d\n",pos);
+					 
+					  for(int j = 0; j < 3; j++){
+					      lastPoints[j] = new Point2D.Double(points[j].getX(), points[j].getY());
+					  }
+					  break;
+					}
+			      }
+			      if( !flag )
+			      {
+				selectedFigure = null;
+				auxFigure = null;
+			      }
+			 }
+			  
 			  repaint();
 			 }
 			 
 			 public void mouseReleased(MouseEvent evt)
 			 {
-					
+				pos = -1;	
 			 }
 		}
 		);
 		
 		this.addMouseMotionListener(
 			new MouseMotionAdapter(){
-				public void mouseDragged(MouseEvent evt){
-   					if( selectedFigure!=null)
-   					{
-			   		Point currentPt = evt.getPoint();
-			   		selectedFigure.x = (int)currentPt.getX();
-			   		selectedFigure.y = (int)currentPt.getY();
-			   		
-			   		prevPt = currentPt;
+				public void mouseDragged(MouseEvent evt)
+				{
+					Point currentPt = evt.getPoint();
+   					
+			   		if (pos == -1)
+        					return;
+        				if( pos != 2)
+        				{
+        					
+          					points[pos].x = currentPt.x; 
+          					points[pos].y = currentPt.y;
+          					
+          					int otherEnd = (pos==1)?2:1;
+          					
+          					//System.out.printf("otherEnd %d\n",otherEnd);
+          					
+          					double newPoint2X = points[otherEnd].getX() + (points[pos].getX() - points[otherEnd].getX())/2;
+          					double newPoint2Y = points[otherEnd].getY() + (points[pos].getY() - points[otherEnd].getY())/2;
+          
+          					points[2].x = newPoint2X; 
+          					points[2].y = newPoint2Y;
+          					
+				   		selectedFigure.w = (int)Math.abs(points[1].getCenterX()-points[0].getCenterX());
+				   		selectedFigure.h = (int)Math.abs(points[1].getCenterY()-points[0].getCenterY());
+        				}
+        				else
+        				{
+        					//System.out.printf("Drag %d\n",pos);
+        					
+        					Double deltaX = currentPt.x - lastPoints[2].getX();
+          					Double deltaY = currentPt.y - lastPoints[2].getY();
+          					
+				   		 for(int j = 0; j < 3; j++){
+				   		 	points[j].x = lastPoints[j].getX() + deltaX;
+				   		 	points[j].y = lastPoints[j].getY() + deltaY;
+				   		 }
+				   		selectedFigure.x = (int)points[0].getCenterX();
+				   		selectedFigure.y = (int)points[0].getCenterY();
+				   		
+        				}
+        				
 			   		repaint();
-			   		}
-			   	}  
+			   	
+			   }  
 			  
 			  public void mouseMoved(MouseEvent evt)
 			  {
 			  pointOfmouse = evt.getPoint();
 			  prevPt = pointOfmouse;
-			  for( Figure fig: fg)
-			  {
-				if( (prevPt.getX()>=fig.x && prevPt.getX()<=fig.x+fig.w) && (prevPt.getY()>=fig.y && prevPt.getY()<=fig.y+fig.h) ){
+			  
+			  ListIterator<Figure> li = fg.listIterator(fg.size());
+			  Figure fig = null;
+			  
+			 while( li.hasPrevious() )
+			 {
+			 	fig = li.previous();
+            			if( (prevPt.getX()>=fig.x && prevPt.getX()<=fig.x+fig.w) && (prevPt.getY()>=fig.y && prevPt.getY()<=fig.y+fig.h) ){
 					auxFigure = fig;
 					repaint();
 					break;
-			    }else{
-			    	auxFigure = null;
-			    }
-			  }
-			  
+			    	}
+			    	else
+			    	{
+			    		auxFigure = null;
+			    	}
+        		 }
 			  repaint();
 			}
 		   }
@@ -155,6 +288,7 @@ class ListFrame extends JFrame
 	public void paint(Graphics g)
 	{
 		super.paint(g);
+
 		for( Figure fig: this.fg)
 		{
 			fig.paint(g);
@@ -183,9 +317,17 @@ class ListFrame extends JFrame
 		Graphics2D g2d = (Graphics2D) g;
 		BasicStroke bs1 = new BasicStroke(3, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER);
 		g2d.setStroke(bs1);
+		
+		int x,y; int w,h;
+		x = selectedFigure.x; y = selectedFigure.y;
+		w = selectedFigure.w; h = selectedFigure.h;
+		points[0].x = (double)x-SIZE; points[0].y = (double)y-SIZE;
+		points[1].x = (double)x+w; points[1].y = (double)y+h; 
+		points[2].x = (double)((x+w)+(x-SIZE))/2; points[2].y = (double)((y+h)+y-SIZE)/2;
 	
 		g2d.setColor(Color.BLUE);
-		
-		g2d.drawRect(selectedFigure.x-5,selectedFigure.y-5, selectedFigure.w+10,selectedFigure.h+10);
+		for (int i = 0; i < points.length; i++) {
+      			g2d.fill(points[i]);
+   		}
 	}
 }
---
+++ README.md
@@ -0,0 +1,23 @@
+Editor Gráfico Vetorial, primeiro projeto de LP2 - PAE 3 - UERJ - 2021.1
+==========================================================================
+
+#### figuras:
+	1. Retangulo
+	2. Circulo
+	3. Retangulo com cantos redondos
+	4. Triangulo equilatero
+#### Comandos:
+	**Adicionar figura**
+		1. tecla r Retangulo;
+		2. tecla e Circulo;
+		3. tecla w Retangulo com cantos redondos;
+		4. tecla t Triangulo equilatero;
+	**Remover figura**
+		1. tecla x ou DELETE;
+	**Mover figura**
+		1.Clicar no pequeno quadrado azul, no centro da figura selecionada, e arrastar. Ou usar as teclas direcionais;
+	**Editar propriedades**
+		1. cor de fundo, tecla b;
+		2. cor de contorno, tecla f;
+		3. alterar o tamanho, arratar os pequenos quadrados azuis no canto superior esquerdo 
+			ou no canto inferior direito da figura selecionada;
